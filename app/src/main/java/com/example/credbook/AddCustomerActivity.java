2fb44b0f485ae9b75968aeec10056337cf5bdb96
Sooraj S Nair <nairsooraj2000@gmail.com>
2020-11-27 01:34:29 +0530

    - add AddCustomerActivity.java and activity_add_customer.xml - add contact.xml for contact list item layout

---
+++ app/src/main/java/com/example/credbook/AddCustomerActivity.java
@@ -0,0 +1,37 @@
+package com.example.credbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.Menu;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+public class AddCustomerActivity extends AppCompatActivity {
+    ListView contactList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_customer);
+        setTitle("Add Customer from Contacts");
+        contactList = findViewById(R.id.contact_list);
+        get();
+
+    }
+
+    public void get(){
+        Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null);
+        startManagingCursor(cursor);
+        String[] from = {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone._ID};
+        int[] to = {android.R.id.text1,android.R.id.text2};
+        SimpleCursorAdapter simpleCursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2,cursor,from,to);
+        contactList.setAdapter(simpleCursorAdapter);
+        contactList.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+    }
+}
--- app/src/main/java/com/example/credbook/DashboardActivity.java
+++ app/src/main/java/com/example/credbook/DashboardActivity.java
@@ -2,8 +2,10 @@ package com.example.credbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 
 public class DashboardActivity extends AppCompatActivity {
     SharedPreferences userPref;
@@ -14,6 +16,11 @@ public class DashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
         userPref = getSharedPreferences("user",MODE_PRIVATE);
         setTitle("CredBook > " + userPref.getString("username",null));
+    }
 
+    public void goToAddCustomerActivity(View view){
+        System.out.println("Clicked");
+        Intent goToAddCustomerActivity = new Intent(DashboardActivity.this,AddCustomerActivity.class);
+        startActivity(goToAddCustomerActivity);
     }
 }
