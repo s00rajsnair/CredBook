783c06bc71166b6d423442e603b77e9c4ea63fb4
Sooraj S Nair <nairsooraj2000@gmail.com>
2020-11-29 02:20:12 +0530

    - add sort to contacts - display number along with contact name

--- app/src/main/java/com/example/credbook/AddCustomerActivity.java
+++ app/src/main/java/com/example/credbook/AddCustomerActivity.java
@@ -7,91 +7,39 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.ContentResolver;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.database.MatrixCursor;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 
 
 import android.provider.ContactsContract;
-import android.view.Menu;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class AddCustomerActivity extends AppCompatActivity {
     ListView contactList;
-    ArrayList arr;
+    ArrayList<HashMap<String, String>> contacts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_add_customer);
         setTitle("Add Customer from Contacts");
-        if(ActivityCompat.checkSelfPermission(AddCustomerActivity.this, Manifest.permission.READ_CONTACTS)== PackageManager.PERMISSION_GRANTED){
-            arr= getAllContact();
-        }
-        else{
+        if (ActivityCompat.checkSelfPermission(AddCustomerActivity.this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+            contacts = getAllContacts();
+        } else {
             requestPermission();
-        }
 
+        }
         contactList = findViewById(R.id.contact_list);
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, arr);
+        ListAdapter adapter = new SimpleAdapter(AddCustomerActivity.this, contacts, R.layout.contact, new String[]{"name", "phno"}, new int[]{R.id.contact_name, R.id.contact_number});
         contactList.setAdapter(adapter);
-
-       
-
-
-
-
-    }
-
-    private ArrayList getAllContact() {
-
-        ArrayList<String> nameList = new ArrayList<>();
-        ContentResolver cr = getContentResolver();
-        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
-                null, null, null, null);
-        if ((cur != null ? cur.getCount() : 0) > 0) {
-            while (cur != null && cur.moveToNext()) {
-                String id = cur.getString(
-                        cur.getColumnIndex(ContactsContract.Contacts._ID));
-                String name = cur.getString(cur.getColumnIndex(
-                        ContactsContract.Contacts.DISPLAY_NAME));
-                nameList.add(name);
-                if (cur.getInt(cur.getColumnIndex( ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
-                    Cursor pCur = cr.query(
-                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                            null,
-                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
-                            new String[]{id}, null);
-                    while (pCur.moveToNext()) {
-                        String phoneNo = pCur.getString(pCur.getColumnIndex(
-                                ContactsContract.CommonDataKinds.Phone.NUMBER));
-                    }
-                    pCur.close();
-                }
-            }
-        }
-        if (cur != null) {
-            cur.close();
-        }
-        return nameList;
-    }
-
-    private void requestPermission() {
-        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)){
-
-        }
-        else{
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_CONTACTS}, 79);
-        }
 
     }
 
@@ -102,13 +50,58 @@ public class AddCustomerActivity extends AppCompatActivity {
         switch (requestCode) {
             case 79: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    arr = getAllContact();
+                    contacts = getAllContacts();
                 } else {
                     Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
                 }
                 return;
             }
         }
+    }
+
+    public  ArrayList<HashMap<String, String>> getAllContacts() {
+
+        ArrayList<HashMap<String, String>> contactList = new ArrayList<>();
+        ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, "upper(" + ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + ") ASC");
+        if ((cur != null ? cur.getCount() : 0) > 0) {
+            while (cur.moveToNext()) {
+                HashMap<String, String> contact = new HashMap<>();
+                String id = cur.getString(
+                        cur.getColumnIndex(ContactsContract.Contacts._ID));
+                contact.put("id", id);
+                String name = cur.getString(cur.getColumnIndex(
+                        ContactsContract.Contacts.DISPLAY_NAME));
+                contact.put("name", name);
+                Cursor pCur = cr.query(
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        null,
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                        new String[]{id}, null);
+                while (pCur.moveToNext()) {
+                    String phoneNo = pCur.getString(pCur.getColumnIndex(
+                            ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    contact.put("phno", phoneNo);
+                }
+                contactList.add(contact);
+                System.out.println(contact);
+            }
+        }
+        if (cur != null) {
+            cur.close();
+        }
+        return contactList;
+    }
+
+
+    public void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
+
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_CONTACTS}, 79);
+        }
+
     }
 }
 
